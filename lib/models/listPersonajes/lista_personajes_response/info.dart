5cd599e3de9dbc4fa0a14072a41cc30473945ea0
mgaangel10 <mg.aangel10@gmail.com>
2024-02-06 14:28:06 +0100

    avances

---
+++ lib/models/listPersonajes/lista_personajes_response/info.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+class Info {
+	int? count;
+	int? pages;
+	String? next;
+	dynamic prev;
+
+	Info({this.count, this.pages, this.next, this.prev});
+
+	factory Info.fromMap(Map<String, dynamic> data) => Info(
+				count: data['count'] as int?,
+				pages: data['pages'] as int?,
+				next: data['next'] as String?,
+				prev: data['prev'] as dynamic,
+			);
+
+	Map<String, dynamic> toMap() => {
+				'count': count,
+				'pages': pages,
+				'next': next,
+				'prev': prev,
+			};
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [Info].
+	factory Info.fromJson(String data) {
+		return Info.fromMap(json.decode(data) as Map<String, dynamic>);
+	}
+  /// `dart:convert`
+  ///
+  /// Converts [Info] to a JSON string.
+	String toJson() => json.encode(toMap());
+}
