9040cade8cf749b165369ff350e5c2b8e56f58bb
Javier Prieto <jpojaviprior@gmail.com>
2024-02-06 12:38:21 +0100

    navigation bar complete

---
+++ lib/ui/episodes_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EpisodeScreen extends StatefulWidget {
+  const EpisodeScreen({super.key});
+
+  @override
+  State<EpisodeScreen> createState() => _EpisodeScreenState();
+}
+
+class _EpisodeScreenState extends State<EpisodeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('episode');
+  }
+}
